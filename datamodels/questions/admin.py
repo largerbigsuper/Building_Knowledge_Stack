bc46d94a1b5cdc2c25c8156c72c865b5dcced379
Frankie <zaihuazhao@163.com>
2019-06-17 23:08:34 +0800

    add admin

--- datamodels/questions/admin.py
+++ datamodels/questions/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from datamodels.questions.models import Question
+
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    pass
+
+
--- datamodels/subjects/admin.py
+++ datamodels/subjects/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from datamodels.subjects.models import Subject, SubjectTerm, Application
+
+@admin.register(Subject)
+class SubjectAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(SubjectTerm)
+class SubjectTermAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Application)
+class ApplicationAdmin(admin.ModelAdmin):
+    pass
+
+

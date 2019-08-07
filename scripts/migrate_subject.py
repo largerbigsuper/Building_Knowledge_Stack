52d9bdfa4b40d1dc7966253d88bf73e69012870d
Frankie <zaihuazhao@163.com>
2019-08-07 18:37:29 +0800

    fix migrate script

--- scripts/migrate_subject.py
+++ scripts/migrate_subject.py
@@ -2,6 +2,7 @@
 from datamodels.subjects.models import mm_Subject
 
 
+
 def run():
 
     name = '技工证'
@@ -9,6 +10,6 @@ def run():
     level_1_subjects = mm_Subject.filter(level=1, parent=subject)
     for p in level_1_subjects:
         print(p.name)
-    level_2_subjects = mm_Subject.filter(level=2).update(parent=subject)
-    level_1_subjects.delete()
+    level_2_subjects = mm_Subject.filter(level=2).update(parent=subject, level=1)
+    # level_1_subjects.delete()
     
